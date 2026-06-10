b3627f9627ca2c365fc46b2b4015b4f67c1284a7
sandhya76-stack <sandhya76stack@gmail.com>
2026-06-10 11:48:56 +0530

    Fix PostgreSQL SSL connection

--- config/db.js
+++ config/db.js
@@ -1,24 +1,8 @@
-// const { Pool } = require('pg');
-// require('dotenv').config();
-
-// const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_NAME,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-// });
-
-// module.exports = pool;
-
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
